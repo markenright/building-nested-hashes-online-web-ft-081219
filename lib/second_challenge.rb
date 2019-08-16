16e0a949c730632d467211f9789b48c62c300574
Mark Enright <enright.mark@gmail.com>
2019-08-16 14:39:03 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {}
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
       
       
       }, 
